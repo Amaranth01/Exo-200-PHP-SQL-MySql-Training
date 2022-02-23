fa63fa55e6126ea94bc23706b0f1b3841926e525
Amaranth01 <vanessa.nectoux@gmail.com>
2022-02-23 12:02:31 +0100

    creat ok

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Randonnées, ajout</title>
 </head>
 <body>
-    <form action="">
+    <form action="" method="post">
         <input type="text" name="name" placeholder="Nom de la randonnée">
         <select name="difficulty" id="difficulty">
             <option value="très facile">Très facile</option>
@@ -17,13 +17,21 @@
             <option value="très difficile">Très difficile</option>
         </select>
         <input type="number" name="distance">
-        <input type="time" name="Durée">
+        <input type="number" name="Durée">
         <input type="number" name="height_difference">
+
+        <input type="submit" value="Envoyer">
     </form>
 </body>
 </html>
 
 <?php
+    $name = trim(strip_tags($_POST['name']));
+    $difficulty = trim(strip_tags($_POST['difficulty']));
+    $distance = trim(strip_tags($_POST['distance']));
+    $duration = trim(strip_tags($_POST['duration']));
+    $height = trim(strip_tags($_POST['height_difference']));
+
 try {
     $server = 'localhost';
     $db = 'exo_200';
@@ -32,23 +40,21 @@ try {
 
     $bdd = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pswd);
 
-    $nom = strip_tags(trim('name'));
-    $difficulty = strip_tags(trim('difficulty'));
-    $distance = strip_tags(trim('distance'));
-    $duration = strip_tags(trim('duration'));
-    $height = strip_tags(trim('height_difference'));
-
     $stm = $bdd->prepare("
         INSERT INTO hiking (name, difficulty, distance, duration, height_difference)
         VALUES (:name, :difficulty, :distance, :duration, :height_difference)
     ");
 
-    $stm->bindParam(':name', $name);
-    $stm->bindParam(':difficulty', $difficulty);
-    $stm->bindParam(':duration', $duration);
-    $stm->bindParam(':height_difference', $height);
 
-    $stm->execute();
+    $stm->bindParam(':name', $name);
+    $stm->bindParam(':difficulty', $difficulty,);
+    $stm->bindParam(':distance', $distance, PDO::PARAM_INT);
+    $stm->bindParam(':duration', $duration);
+    $stm->bindParam(':height_difference', $height, PDO::PARAM_INT);
+
+    if ($stm->execute()){
+        echo "rando ajoutée";
+    }
 }
 catch (PDOException $e){
     echo $e->getMessage();
--- read.php
+++ read.php
@@ -10,11 +10,34 @@ try {
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    $stm = $bdd->prepare("SELECT name FROM hiking");
+    $stm = $bdd->prepare("SELECT * FROM hiking");
 
     if($stm->execute()){
+        ?>
+        <table>
+            <thead>
+            <tr>
+                <td>Nom</td>
+                <td>Difficulté</td>
+                <td>Distance</td>
+                <td>Durée</td>
+                <td>Dénivelé</td>
+            </tr>
+            </thead>
+        <?php
         foreach ($stm->fetchAll() as $item) {
-            echo "Nom de la randonnée : " . $item['name']  ."<br>";
+        ?>
+            <tbody>
+            <tr>
+                <td><?= $item['name'] ?></td>
+                <td><?= $item['difficulty'] ?></td>
+                <td><?= $item['distance'] ?></td>
+                <td><?= $item['duration'] ?></td>
+                <td><?= $item['height_difference'] ?></td>
+            </tr>
+            </tbody>
+
+            <?php
         }
     }
 }
